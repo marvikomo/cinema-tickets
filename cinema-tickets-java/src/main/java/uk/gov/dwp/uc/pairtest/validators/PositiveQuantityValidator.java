cd32f04a02e08e7c4855ee901cb7c752b02cc69d
marvikomo <marvikomo@gmail.com>
2025-04-01 15:31:27 +0100

    setup PositiveQuantityValidator

---
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/validators/PositiveQuantityValidator.java
@@ -0,0 +1,14 @@
+package uk.gov.dwp.uc.pairtest.validators;
+
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+
+public class PositiveQuantityValidator implements TicketValidator{
+    /**
+     * @param accountId
+     * @param requests
+     */
+    @Override
+    public void validate(Long accountId, TicketTypeRequest... requests) {
+
+    }
+}
