6a5a3aee1a5d4e7fb7075bdab13237271610a6d8
marvikomo <marvikomo@gmail.com>
2025-04-01 14:40:36 +0100

    update

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/validators/AccountValidator.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/validators/AccountValidator.java
@@ -1,10 +1,15 @@
 package uk.gov.dwp.uc.pairtest.validators;
 
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 public class AccountValidator implements TicketValidator{
     @Override
     public void validate(Long accountId, TicketTypeRequest... requests) {
+
+        if(accountId == null) {
+            throw new InvalidPurchaseException("Invalid account ID.", InvalidPurchaseException.ValidationFailureType.INVALID_ACCOUNT_ID);
+        }
 
     }
 }
