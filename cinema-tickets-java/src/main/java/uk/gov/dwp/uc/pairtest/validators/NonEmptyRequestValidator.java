a3362e22948c7e4a21b476e1ddfa203d3e82bab7
marvikomo <marvikomo@gmail.com>
2025-04-01 15:22:58 +0100

    update

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/validators/NonEmptyRequestValidator.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/validators/NonEmptyRequestValidator.java
@@ -1,4 +1,14 @@
 package uk.gov.dwp.uc.pairtest.validators;
 
-public class NonEmptyRequestValidator {
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+
+public class NonEmptyRequestValidator implements TicketValidator{
+    /**
+     * @param accountId
+     * @param requests
+     */
+    @Override
+    public void validate(Long accountId, TicketTypeRequest... requests) {
+
+    }
 }
