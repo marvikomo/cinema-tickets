2ffcb155fca6c54e1d0db93d2dbf77f5c644cbd0
marvikomo <marvikomo@gmail.com>
2025-04-01 16:24:31 +0100

    impl TicketRequestValidator

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/validators/TicketRequestValidator.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/validators/TicketRequestValidator.java
@@ -2,9 +2,21 @@ package uk.gov.dwp.uc.pairtest.validators;
 
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 
-public class TicketRequestValidator implements TicketValidator{
-    @Override
-    public void validate(Long accountId, TicketTypeRequest... requests) {
+import java.util.List;
 
+public class TicketRequestValidator{
+    private static final List<TicketValidator> VALIDATORS = List.of(
+            new NonEmptyRequestValidator(),
+            new AccountValidator(),
+            new PositiveQuantityValidator(),
+            new TicketCountValidator(),
+            new AdultPresenceValidator(),
+            new InfantAdultRatioValidator()
+    );
+
+    public static void validate(Long accountId, TicketTypeRequest... requests) {
+        for (TicketValidator validator : VALIDATORS) {
+            validator.validate(accountId, requests);
+        }
     }
 }
